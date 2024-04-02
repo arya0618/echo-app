56ade165e541ae6410d0f14c5f9a166142a0a449
arya0618 <piyusha208@gmail.com>
2024-04-02 20:32:47 +0530

    base file setup

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const Koa = require('koa');
+const createRoutes = require('./router');
+const bodyParser = require('koa-bodyparser');
+
+// Create a Koa app
+const app = new Koa();
+
+app.use(bodyParser());
+
+
+// Use the routes middleware
+app.use(createRoutes);
+
+// Start the Koa server
+const PORT=4000
+const server = app.listen(PORT);
+
+console.log(`Koa app running on http://localhost:${PORT}`);
